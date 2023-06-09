48090ac7e7b3e593692f36c2e7e2434f5e947b8d
Gabe Montague <montague.gabe@gmail.com>
2023-06-09 11:20:59 -0400

    Delete test

--- src/parsagon/tests/test_selenium.py
+++
@@ -1,7 +0,0 @@
-from src.parsagon.selenium_wrapper import SeleniumWrapper
-
-
-def test_selenium():
-    wrapper = SeleniumWrapper()
-    wrapper.goto("https://gabemontague.com/example/")
-    wrapper.wait(5)

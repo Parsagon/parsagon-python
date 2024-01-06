1c91369f5a313c01f5a5fdb2b14721a32dc311b5
Gabe Montague <montague.gabe@gmail.com>
2024-01-06 16:01:10 -0500

    Update

--- gui/install_cert.sh
+++ gui/install_cert.sh
@@ -2,26 +2,26 @@
 
 set -euo pipefail
 
-# Usage: ./install_cert.sh [Base64 Encoded Certificate] [Certificate Password] [Keychain Password] [Keychain Name]
+APP_CERT=$1
+INSTALLER_CERT=$2
+SECURE_PASSWORD=$3
+KEYCHAIN_NAME="builder"
 
-ENCODED_CERT=$1
-CERT_PASSWORD=$2
-KEYCHAIN_PASSWORD=$3
-KEYCHAIN_NAME=$4
-
-# Decode the certificate and save to a temporary file
-echo "$ENCODED_CERT" | base64 --decode > /tmp/certificate.p12
+# Decode the certificates and save to temporary files
+echo "$APP_CERT" | base64 --decode > /tmp/certificate_app.p12
+echo "$INSTALLER_CERT" | base64 --decode > /tmp/certificate_installer.p12
 
 # Create a keychain
 security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
 security default-keychain -s "$KEYCHAIN_NAME"
-security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
+security unlock-keychain -p "$SECURE_PASSWORD" "$KEYCHAIN_NAME"
 
-# Import the certificate into the keychain
-security import /tmp/certificate.p12 -k "$KEYCHAIN_NAME" -P "$CERT_PASSWORD" -T /usr/bin/codesign
+# Import the certificates into the keychain
+security import /tmp/certificate_app.p12 -k "$KEYCHAIN_NAME" -P "$SECURE_PASSWORD" -T /usr/bin/codesign
+security import /tmp/certificate_installer.p12 -k "$KEYCHAIN_NAME" -P "$SECURE_PASSWORD" -T /usr/bin/codesign
 
 # Set key partition list
-security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
+security set-key-partition-list -S apple-tool:,apple: -s -k "$SECURE_PASSWORD" "$KEYCHAIN_NAME"
 
 # Add the keychain to the list of search keychains
 security list-keychains -s "$KEYCHAIN_NAME"

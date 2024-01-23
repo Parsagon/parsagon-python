ea6c531b0dbf756c576fa704e2fd2328999fb187
Sandy Suh <suh.sandy@gmail.com>
2024-01-23 02:45:11 -0800

    fix bug with getting visible html

--- src/parsagon/executor.py
+++ src/parsagon/executor.py
@@ -219,19 +219,29 @@ class Executor:
                 id_to_elem[elem_id] = elem
         visible_elem_ids = set(
             driver.execute_script(
-                "return Array.from(document.getElementsByTagName('*')).filter((elem) => { const style = getComputedStyle(elem); return style.opacity > 0.1 && style.display !== 'none' && style.visibility === 'visible' && elem.offsetWidth && elem.offsetHeight && elem.getClientRects().length }).map((elem) => elem.getAttribute('data-psgn-id'))"
+                "return Array.from(document.getElementsByTagName('*')).filter((elem) => { const style = getComputedStyle(elem); const rect = elem.getBoundingClientRect(); return style.opacity > 0.1 && style.display !== 'none' && style.visibility === 'visible' && elem.offsetWidth && elem.offsetHeight && elem.getClientRects().length && rect.right + window.scrollX >= 0 && rect.bottom + window.scrollY >= 0 }).map((elem) => elem.getAttribute('data-psgn-id'))"
             )
         )
         max_elem_id = self.max_elem_ids[self.driver.current_window_handle]
         with Progress() as progress:
             for elem_id in progress.track(range(max_elem_id), description="[green]Analyzing page"):
                 elem_id = str(elem_id)
-                if elem_id in id_to_elem and elem_id not in visible_elem_ids:
-                    lxml_elem = id_to_elem[elem_id]
-                    parent = lxml_elem.getparent()
-                    if parent is not None:
-                        parent.remove(lxml_elem)
-
+                is_visible = elem_id in visible_elem_ids
+                if not is_visible:
+                    try:
+                        lxml_elem = root.xpath(f'//*[@data-psgn-id="{elem_id}"]')[0]
+                        contains_visible = False
+                        for descendant in lxml_elem.iterdescendants():
+                            if descendant.get("data-psgn-id") in visible_elem_ids:
+                                contains_visible = True
+                                break
+                        if contains_visible:
+                            continue
+                        parent = lxml_elem.getparent()
+                        if parent is not None:
+                            parent.remove(lxml_elem)
+                    except IndexError:
+                        continue
         return lxml.html.tostring(root).decode()
 
     def get_elem(self, description, elem_type):

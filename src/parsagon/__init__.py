15623355bdbcac098ec4e645eaca8e4be7e9ef7e
Sandy Suh <suh.sandy@gmail.com>
2024-01-28 22:28:23 -0800

    add more utilities

--- src/parsagon/__init__.py
+++ src/parsagon/__init__.py
@@ -1,5 +1,5 @@
 from parsagon.main import update, detail, delete, get_product, get_review_article, get_article_list
 from parsagon.create import create_program as create
 from parsagon.runs import run, batch_runs
-from parsagon.api import get_json_about_data
+from parsagon.api import get_str_about_data, get_bool_about_data, get_json_about_data
 from parsagon.assistant import get_page_html, get_page_text

9f4845e3ada2f6a7b77a7777e0e08270a4beec3e
Sandy Suh <suh.sandy@gmail.com>
2024-03-14 00:32:48 -0700

    allow user to add any chrome options they want; make get_pipeline accessible

--- src/parsagon/__init__.py
+++ src/parsagon/__init__.py
@@ -1,6 +1,6 @@
 from parsagon.main import update, detail, delete, get_product, get_review_article, get_article_list
 from parsagon.create import create_program as create
 from parsagon.runs import run, batch_runs
-from parsagon.api import get_str_about_data, get_bool_about_data, get_json_about_data
+from parsagon.api import get_str_about_data, get_bool_about_data, get_json_about_data, get_pipeline
 from parsagon.assistant import get_page_html, get_page_text
 from parsagon.executor import Executor
